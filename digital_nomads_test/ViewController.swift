4d13ec1e68d247052da7ec1d4aa155220126b027
Aleksandr Chegoshev <achegoshev@dnomads.pro>
2020-04-26 22:44:57 +0700

    Initial Commit

---
+++ digital_nomads_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  digital_nomads_test
+//
+//  Created by Александр Чегошев on 26.04.2020.
+//  Copyright © 2020 Aleksandr Chegoshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
